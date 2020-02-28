b98be599514fd8cbfbdda1a93e46c1238193f6cf
Administrator <kevingu462@gmail.com>
2020-02-28 11:51:38 +0800

    代码提交

--- app/src/main/java/com/dejin/urltest/activity/UrlEncodeActivity.kt
+++ app/src/main/java/com/dejin/urltest/activity/UrlEncodeActivity.kt
@@ -13,16 +13,28 @@ import kotlinx.android.synthetic.main.activity_url_encode.*
 import org.litepal.util.cipher.AESCrypt
 
 class UrlEncodeActivity : AppCompatActivity() {
-    var key = "U2FsdGVkX19X5NAwZN8I2oWsRPaYGJPy"
+    private var key = "U2FsdGVkX19X5NAwZN8I2oWsRPaYGJPy"//淘金岛  钱多多
+    // private var key = "nrVvOYKC3yYiZRd13QjSQbTKGlXzLGaA"//人人赚
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_url_encode)
         et_urls.setText(urls)
     }
 
+    //淘金岛
+    /*  private val urls =
+          """["https://balance.leffood.com","https://ers.letongfushi.com","https://ers.leffood.com","https://ers.zzzxqyfwpt.com","https://ers.xxxhzxx.com",
+              "https://api.wumiicdn.com","https://api.quepinget.com","https://api.bjbhxykj.com","https://api.4000700917.com","https://duoyixin.com","https://ccduobao.com",
+              "https://nmgxykaolin.com","https://xinjiangauto.com","https://api.sygcsx.com","https://go.echaeke.com","https://yd.marxns.com",
+              "https://lt.yuanqiguo.com","https://df.hfzgfwbfzx.com"]""".trimIndent()*/
 
+    //钱多多
     private val urls =
-        """["https://balance.leffood.com","https://ers.letongfushi.com","https://ers.leffood.com","https://ers.zzzxqyfwpt.com","https://ers.xxxhzxx.com","https://api.wumiicdn.com","https://api.quepinget.com","https://api.bjbhxykj.com","https://api.4000700917.com","https://duoyixin.com","https://ccduobao.com","https://nmgxykaolin.com","https://xinjiangauto.com","https://api.sygcsx.com","https://go.echaeke.com","https://yd.marxns.com","https://lt.yuanqiguo.com","https://df.hfzgfwbfzx.com"]""".trimIndent()
+        """["https://api.islandke.com","https://api.zhangjiandev.com","https://api.miyuhao.com","https://api.yuanwu123.com","https://api.ddline3765.shop","https://api.qdd6541.cn","https://api.qdd6540.cn","https://api.qdd6542.cn"]""".trimIndent()
+    //人人赚
+//    private val urls =
+//        """["https://api.qdd4356.club","https://api.zdd1435.ltd","https://api.zdd8758.net","https://api.zdd8989.com","https://api.ddline3748.com"]""".trimIndent()
 
     fun startEncode(view: View) {
         val urls = et_urls.text.toString().trim()
